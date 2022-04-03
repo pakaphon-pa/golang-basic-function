c37b8dbb0d64ebbc1786dee982dc91f6a25ee3bc
dackmagic115 <pakaphon23@gmail.com>
2022-04-03 11:00:50 +0700

    add test case

--- palindrome/palindrome_test.go
+++ palindrome/palindrome_test.go
@@ -7,7 +7,19 @@ import (
 )
 
 func TestIsPalindromeString(t *testing.T) {
-	result := CheckPalindrome("10001")
+	result := CheckPalindrome("ABABA")
+
+	assert.Equal(t, true, result)
+}
+
+func TestIsPalindromeStringFalse(t *testing.T) {
+	result := CheckPalindrome("TIME")
+
+	assert.Equal(t, false, result)
+}
+
+func TestIsPalindromeInt(t *testing.T) {
+	result := CheckPalindrome(10001)
 
 	assert.Equal(t, true, result)
 }
